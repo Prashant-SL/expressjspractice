d80d6c61be25c741fbe4090659b9a0e95685d3a1
Prashant-SL <pacchidon07@gmail.com>
2022-07-13 23:24:57 +0530

    view engine

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ const app = express();
 const path = require("path");
 
 const pathbro = path.join(__dirname, "../public");
-app.use(express.static(pathbro));
+// app.use(express.static(pathbro));
+
+app.set("view engine", "hbs");
+app.get("/", (req, res) => {
+    res.render("test");
+})
 
 app.get("/", (req, res) => {
-    // res.write("<h1> Hiee Brooo</h1>");
-    // res.write("<p>how are you??</p>");
-    // res.send();
-    res.send({
-        key1: "value1"
-    })
+    res.send()
 })
 
 app.listen(4000, () => {
