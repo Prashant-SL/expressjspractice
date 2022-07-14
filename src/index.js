baa08605b6eb37380a6898b649ba42dc2c20cc4a
Prashant-SL <pacchidon07@gmail.com>
2022-07-14 18:10:48 +0530

    view engine hbs

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const pathbro = path.join(__dirname, "../public");
+// const pathbro = path.join(__dirname, "../public");
 // app.use(express.static(pathbro));
 
 app.set("view engine", "hbs");
-app.get("/", (req, res) => {
-    res.render("test");
-})
 
 app.get("/", (req, res) => {
-    res.send()
-})
+    res.render("best");
+});
+
+app.get("/", (req, res) => {
+    res.send("Bro")
+});
 
 app.listen(4000, () => {
     console.log("Im running on port 4000");
